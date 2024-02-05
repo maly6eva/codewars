39aab7c6cbb250e27ba9f9e3923f135ba266e76b
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-02-05 23:42:22 +0300

    If you can't sleep, just count sheep!!

---
+++ 8kyu/If you can't sleep, just count sheep!!/index.js
@@ -0,0 +1,7 @@
+function countSheep(num) {
+    let sheep = "";
+    for (let i = 1; i <= num; i++) {
+        sheep += `${i} sheep...`;
+    }
+    return sheep;
+}
---
+++ 8kyu/If you can't sleep, just count sheep!!/task.test.js
@@ -0,0 +1,8 @@
+describe('Tests', () => {
+    it('countSheep', () => {
+        expect(countSheep(0)).toBe("");
+        expect(countSheep(1)).toBe("1 sheep...");
+        expect(countSheep(2)).toBe("1 sheep...2 sheep...");
+        expect(countSheep(3)).toBe("1 sheep...2 sheep...3 sheep...");
+    });
+});
