6c6d3f9b183840e0917ee844482563a75c59d02b
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-02-11 00:04:44 +0300

    Get number from string

---
+++ 8kyu/Get number from string/task.test.js
@@ -0,0 +1,7 @@
+describe('Tests', () => {
+    it('getNumberFromString', () => {
+        expect(getNumberFromString("1")).toBe(1);
+        expect(getNumberFromString("123")).toBe(123);
+        expect(getNumberFromString("this is number: 7")).toBe(7);
+    });
+});
