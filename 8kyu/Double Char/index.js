146a16a0413f88d8bdbc62b47e7dc67088842bab
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-02-13 00:28:17 +0300

    Double Char

---
+++ 8kyu/Double Char/index.js
@@ -0,0 +1,7 @@
+function doubleChar(str) {
+    let doubleString = '';
+    for (let i = 0; i < str.length; i++) {
+        doubleString += str[i] + str[i];
+    }
+    return doubleString;
+}
---
+++ 8kyu/Double Char/task.test.js
@@ -0,0 +1,10 @@
+describe('Tests', () => {
+    it('doubleChar', () => {
+        expect(doubleChar("abcd")).toBe("aabbccdd");
+        expect(doubleChar("Adidas")).toBe("AAddiiddaass");
+        expect(doubleChar("1337")).toBe("11333377");
+        expect(doubleChar("illuminati")).toBe("iilllluummiinnaattii");
+        expect(doubleChar("123456")).toBe("112233445566");
+        expect(doubleChar("%^&*(")).toBe("%%^^&&**((");
+    });
+});
