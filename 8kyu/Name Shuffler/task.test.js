a460aa4bf5fcd4e4a87f1dbc713e4ec159619912
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-02-14 00:25:10 +0300

    Name Shuffler

---
+++ 8kyu/Name Shuffler/task.test.js
@@ -0,0 +1,8 @@
+describe('Tests', () => {
+    it('nameShuffler', () => {
+
+        expect(nameShuffler('john McClane')).toBe('McClane john');
+        expect(nameShuffler('Mary jeggins')).toBe('jeggins Mary');
+        expect(nameShuffler('tom jerry')).toBe('jerry tom');
+    });
+});
