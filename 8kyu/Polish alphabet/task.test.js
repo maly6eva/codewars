2c6297d2911ecc232eb1224ceb6e0977884c8513
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-02-14 23:40:52 +0300

    Polish alphabet

---
+++ 8kyu/Polish alphabet/task.test.js
@@ -0,0 +1,7 @@
+describe('Tests', () => {
+    it('correctPolishLetters', () => {
+        expect(correctPolishLetters("Jędrzej Błądziński")).toBe("Jedrzej Bladzinski");
+        expect(correctPolishLetters("Lech Wałęsa")).toBe("Lech Walesa");
+        expect(correctPolishLetters("Maria Skłodowska-Curie")).toBe("Maria Sklodowska-Curie");
+    });
+});
