56af86c8e00b2126af9d80579e65a8d6120a358a
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-02-17 01:20:28 +0300

    All Star Code Challenge #18

---
+++ 8kyu/All Star Code Challenge #18/index.js
@@ -0,0 +1,11 @@
+function strCount(str, letter) {
+    let letterCount = 0;
+    for (let i = 0; i < str.length; i++)
+    {
+        if (str.charAt(i) === letter)
+        {
+            letterCount += 1;
+        }
+    }
+    return letterCount;
+}
---
+++ 8kyu/All Star Code Challenge #18/task.test.js
@@ -0,0 +1,7 @@
+describe('Tests', () => {
+    it('strCount', () => {
+        expect(strCount('Hello', 'o')).toBe(1);
+        expect(strCount('Hello', 'l')).toBe(2);
+        expect(strCount('', 'z')).toBe(0);
+    });
+});
