dd78ebfc0438fa9309a357cb8cd926a65473ffe2
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-02-16 18:51:51 +0300

    Sum of Digits / Digital Root

---
+++ 8kyu/Sum of Digits - Digital Root/index.js
@@ -0,0 +1,9 @@
+function digitalRoot(n) {
+    const numberStr = n.toString();
+    if (numberStr.length === 1) {
+        return n;
+    }
+    const numberStrArr = numberStr.split('');
+    const sumNumber = numberStrArr.reduce((a, b) => a + +b, 0);
+    return digitalRoot(sumNumber);
+}
---
+++ 8kyu/Sum of Digits - Digital Root/task.test.js
@@ -0,0 +1,6 @@
+describe('Tests', () => {
+    it('digitalRoot', () => {
+        expect(digitalRoot(16)).toBe(7);
+        expect(digitalRoot(456)).toBe(6);
+    });
+});
