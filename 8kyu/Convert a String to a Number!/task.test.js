a304b1004b3fdc6113217bddf809c24c072db277
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-02-19 23:29:36 +0300

    Convert a String to a Number!

---
+++ 8kyu/Convert a String to a Number!/task.test.js
@@ -0,0 +1,8 @@
+describe('Tests', () => {
+    it('stringToNumber', () => {
+        expect(stringToNumber("1234")).toBe(1234);
+        expect(stringToNumber("605")).toBe(605);
+        expect(stringToNumber("1405")).toBe(1405);
+        expect(stringToNumber("-7")).toBe(-7);
+    });
+});
