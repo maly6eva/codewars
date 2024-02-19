1b7078f5d371a983747a676467733c20d71733a8
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-02-19 23:09:21 +0300

    DNA to RNA Conversion

---
+++ 8kyu/DNA to RNA Conversion/index.js
@@ -0,0 +1,13 @@
+const DNA = 'GCAT';
+const DNAtoRNA = (DNA) => {
+    let res = '';
+    for (let i = 0; i < DNA.length; i++) {
+        if (DNA[i] === "T") {
+            res += "U";
+        } else {
+            res += DNA[i];
+        }
+    }
+    return res;
+};
+console.log(DNAtoRNA(DNA));
