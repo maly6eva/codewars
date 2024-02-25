3075b2b96be4e302cbba7055621b86f328360f49
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-02-26 00:42:56 +0300

    Are You Playing Banjo?

---
+++ 8kyu/Are You Playing Banjo?/task.test.js
@@ -0,0 +1,9 @@
+describe('Tests', () => {
+    it('areYouPlayingBanjo', () => {
+        expect(areYouPlayingBanjo("Adam")).toBe("Adam does not play banjo");
+        expect(areYouPlayingBanjo("Paul")).toBe("Paul does not play banjo");
+        expect(areYouPlayingBanjo("Ringo")).toBe("Ringo plays banjo");
+        expect(areYouPlayingBanjo("bravo")).toBe("bravo does not play banjo");
+        expect(areYouPlayingBanjo("rolf")).toBe("rolf plays banjo");
+    });
+});
